b369f9e68b68c053a846ad0fd0472f0f3692e704
luferagu <luferagu2015@gmail.com>
2026-02-24 11:23:45 -0500

    Update database.py

--- database.py
+++ database.py
@@ -1,18 +1,8 @@
-import psycopg2
+import psycopg
 import streamlit as st
-from urllib.parse import urlparse
 
 def conectar_db():
-    url = urlparse(st.secrets["DATABASE_URL"])
-
-    conn = psycopg2.connect(
-        dbname=url.path[1:],  # quita el '/'
-        user=url.username,
-        password=url.password,
-        host=url.hostname,
-        port=url.port,
-        sslmode="require",
-        options='-c client_encoding=UTF8'
+    return psycopg.connect(
+        st.secrets["DATABASE_URL"],
+        sslmode="require"
     )
-
-    return conn
