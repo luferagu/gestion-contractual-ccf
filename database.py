291a310a14e2397e8bef4a92f8a6ae8a6d7d6d15
luferagu <luferagu2015@gmail.com>
2026-02-24 11:14:27 -0500

    Update database.py

--- database.py
+++ database.py
@@ -2,9 +2,9 @@ import psycopg2
 import streamlit as st
 
 def conectar_db():
-    return psycopg2.connect(st.secrets["DATABASE_URL"])
+    return psycopg2.connect(
+        st.secrets["DATABASE_URL"],
+        sslmode="require"
+    )
 
-def crear_tablas():
-    pass
-pass
 
