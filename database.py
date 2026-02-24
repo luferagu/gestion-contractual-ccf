c790d1e031bb6b429405bbb768762f2eb4ec3dd0
luferagu <luferagu2015@gmail.com>
2026-02-24 08:49:41 -0500

    Update database.py

--- database.py
+++ database.py
@@ -1,41 +1,8 @@
-import sqlite3
-
-DB_NAME = "gestion_contractual.db"
+import psycopg2
+import os
 
 def conectar_db():
-    return sqlite3.connect(DB_NAME, check_same_thread=False)
+    return psycopg2.connect(os.environ["DATABASE_URL"])
 
 def crear_tablas():
-
-    conn = conectar_db()
-    cursor = conn.cursor()
-
-    cursor.execute("""
-    CREATE TABLE IF NOT EXISTS procesos (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        id_proceso TEXT UNIQUE,
-        objeto TEXT,
-        necesidad TEXT,
-        justificacion TEXT,
-        valor REAL,
-        plazo INTEGER,
-        fecha_estudio TEXT
-    )
-    """)
-
-    cursor.execute("""
-    CREATE TABLE IF NOT EXISTS contratos (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        id_proceso TEXT,
-        tipo_contrato TEXT,
-        supervisor TEXT,
-        cdp TEXT,
-        duracion TEXT,
-        empresa TEXT,
-        fecha_firma TEXT,
-        FOREIGN KEY(id_proceso) REFERENCES procesos(id_proceso)
-    )
-    """)
-
-    conn.commit()
-    conn.close()
+    pass
