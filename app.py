57b178bbd9ccbe2d49e44d7aacc7f2765f6d88de
luferagu <luferagu2015@gmail.com>
2026-02-24 11:41:33 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,1 @@
-import streamlit as st
-from database import conectar_db
 
-st.title("PRUEBA DE CONEXIÓN POSTGRESQL")
-
-try:
-    conn = conectar_db()
-    st.success("Conexión exitosa")
-    conn.close()
-except Exception as e:
-    st.error(f"Error de conexión: {e}")
